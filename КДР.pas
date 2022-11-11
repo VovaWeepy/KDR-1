b9d4f9ee781d1625fedcc8a45c397310cad5f050
VovaWeepy <117993619+VovaWeepy@users.noreply.github.com>
2022-11-11 16:29:24 +0300

    Add files via upload

---
+++ КДР.pas
@@ -0,0 +1,16 @@
+﻿var x,a,b,y,h:real;
+begin 
+x:=a;
+h:=0.2;
+readln(a);
+readln (b);
+while x<=b do 
+begin
+if x<-7 then y:=cos(2*x)*4+-x/x
+else if x<1 then y:=power(x,2/tan(x))+power(x,(1/3))
+else if x<2 then y:=power(x,(1/3))*power(e,x)+x/-x
+else y:=(x/cos(x))*(power(x,2/-x));
+writeln('x=',x,' y=',y);
+x:=x+h
+end;
+end.
